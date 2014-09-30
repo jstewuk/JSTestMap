49d5643eb61aff1e02c211f28d059036f03a8eff
Jim Stewart <jim@stewartstuff.com>
2014-09-30 13:59:41 -0500

    Initial Commit

---
+++ JSTestMap/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JSTestMap
+//
+//  Created by Jim on 9/30/14.
+//  Copyright (c) 2014 idev.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JSTestMap/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JSTestMap
+//
+//  Created by Jim on 9/30/14.
+//  Copyright (c) 2014 idev.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
